e6d7865ee781bf07d48f3c9c7acb0ac82086225b
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2022-09-05 15:59:36 -0300

    Details Page

--- lib/components/cart_item_widget.dart
+++ lib/components/cart_item_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/cart_item.dart';
+
+import '../models/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
   final CartItem cartItem;
@@ -10,13 +13,33 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: ListTile(
-        
-        title: Text(cartItem.title),
-        subtitle: Text("Total R\$ ${cartItem.price * cartItem.quantity}"),
-        trailing: Text("${cartItem.quantity}x"),
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+      ),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: ListTile(
+          title: Text(cartItem.title),
+          subtitle: Text("Total R\$ ${cartItem.price * cartItem.quantity}"),
+          trailing: Text("${cartItem.quantity}x"),
+        ),
       ),
     );
   }
