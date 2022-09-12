4f0b7254f7fc52dc47c00aff1230b22a39a36841
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2022-09-12 14:04:05 -0300

    Formulary Finish

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -17,14 +19,26 @@ class ProductItem extends StatelessWidget {
         width: 100,
         child: Row(children: [
           IconButton(
-            icon: Icon(Icons.edit, color: Theme.of(context).colorScheme.primary,),
+            icon: Icon(
+              Icons.edit,
+              color: Theme.of(context).colorScheme.primary,
+            ),
             onPressed: () {
-              Navigator.of(context).pushNamed(AppRoutes.productsForm, arguments: produto);
+              Navigator.of(context)
+                  .pushNamed(AppRoutes.productsForm, arguments: produto);
             },
           ),
           IconButton(
-            icon: const Icon(Icons.delete, color: Colors.red,),
-            onPressed: () {},
+            icon: const Icon(
+              Icons.delete,
+              color: Colors.red,
+            ),
+            onPressed: () {
+              Provider.of<ProductList>(
+                context,
+                listen: false,
+              ).removeProduct(produto);
+            },
           ),
         ]),
       ),
