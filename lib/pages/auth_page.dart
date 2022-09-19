5a4af32af4f6dad8c77af175a98513c792678d57
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2022-09-19 15:23:19 -0300

    Auth Form

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -24,33 +25,34 @@ class AuthPage extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 70, vertical: 10),
-                        transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.deepOrange.shade900,
-                      boxShadow: const [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    child: const Text(
-                      'Minha Loja',
-                      style: TextStyle(
-                          fontSize: 45,
-                          fontFamily: 'Anton',
-                          color: Colors.white),
-                    ),
-                  )
-                ]),
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                        fontSize: 45, fontFamily: 'Anton', color: Colors.white),
+                  ),
+                ),
+                AuthForm(),
+              ],
+            ),
           )
         ],
       ),
